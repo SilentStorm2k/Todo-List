0641089918d947c4da237d80148edccff9455dc7
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-02-12 21:36:07 +0530

    feat: calls render from ui.js to render webpage

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 import 'normalize.css';
 import '../css/styles.css';
 import addFavicon from './favicon';
+import renderUI from './ui';
 
 function init () {
     addFavicon();
+    renderUI();
 }
 
 init();
